cdf7490ba28a1294a6a2f97bf9972a75742d02d5
Thomas Durand <durand.thomas765@gmail.com>
2025-03-12 17:57:05 +0100

    test2

--- JS/script.js
+++ JS/script.js
@@ -2,15 +2,21 @@ document.addEventListener("DOMContentLoaded", () => {
   const joyButton = document.getElementById("joy-button");
   const flowers = document.querySelectorAll(".Flower");
   const sun = document.querySelector(".Sun");
+  const joyContainer = document.querySelector(
+    ".joy-section .emotion-container"
+  );
   const calmeButton = document.getElementById("calme-button");
   const plane = document.querySelector(".Plane");
   const calmText = document.querySelector(".calm-text");
+  const tristesseButton = document.getElementById("tristesse-button");
+  const rainContainer = document.querySelector(".rain-container");
 
   joyButton.addEventListener("click", () => {
     flowers.forEach((flower) => {
       flower.style.display = "flex";
     });
     sun.style.display = "block";
+    joyContainer.classList.add("active");
     joyButton.style.display = "none";
   });
 
@@ -23,4 +29,10 @@ document.addEventListener("DOMContentLoaded", () => {
     }, 100);
     calmeButton.style.display = "none";
   });
+
+  tristesseButton.addEventListener("click", () => {
+    rainContainer.style.display = "block";
+    tristesseButton.style.display = "none";
+  });
 });
+
